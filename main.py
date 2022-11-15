120d719a7a3115a175fb8d11493bf19adda314c7
Harveylijh <jli783@wisc.edu>
2022-11-15 01:49:02 -0800

    setup LOD reducing mechanism

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 from gltflib import GLTF
 import os
-import trimesh
+import open3d as o3d
 import sys
+import copy
+import numpy as np
 
 MESH_NAME = ''
 
 
-def writeToRecord(data, filename=''):
-    f = open(MESH_NAME + '-' + filename+'.txt', 'a')
-    f = open(MESH_NAME + '-' + filename+'.txt', 'w')
-    f.write(formatOutput(str(data))+'\n\n')
-    f.close()
-
-
-def formatOutput(data):
-    return str(data).replace(', ', ',\n').replace('{', '{\n').replace('}', '\n}').replace('[', '[\n').replace(']', '\n]').replace('(', '(\n').replace(')', '\n)')
-
-
-def getFileSize(filename, roundDigit=2):
+def getFileSize(filename, roundDigit=2) -> float:
     size = os.path.getsize(filename)
     if(size == 0):
         return '0 byte'
@@ -39,39 +30,34 @@ def getFileSize(filename, roundDigit=2):
     return str(round(size, roundDigit))+' bytes'
 
 
-def subdivide(mesh, numSubdivisions):
-    if numSubdivisions == 0:
-        return mesh
-    else:
-        numSubdivisions = numSubdivisions - 1
-        return subdivide(mesh.subdivide(), numSubdivisions)
+def exportToGLB(mesh, filename='assets1\\'+MESH_NAME+'-remesh'+'.glb')  -> None:
+    o3d.io.write_triangle_mesh(filename, mesh)
+
+# simplify mesh using Vertex clustering
+# lower the degree value, more the simplification
+def simplifyMeshVC(mesh, degree=32) -> o3d.geometry.TriangleMesh:
+    voxel_size = max(mesh.get_max_bound() - mesh.get_min_bound()) / degree
+    print(f'voxel_size = {voxel_size:e}')
+    mesh_smp = mesh.simplify_vertex_clustering(
+        voxel_size=voxel_size,
+        contraction=o3d.geometry.SimplificationContraction.Average)
+    return mesh_smp
+
+# simplify mesh using Quadric Decimation
+# lower the num_of_triangle value, more the simplification
+def simplifyMeshQD(mesh, num_of_triangles) -> o3d.geometry.TriangleMesh:
+    mesh_smp = mesh.simplify_quadric_decimation(
+        target_number_of_triangles=num_of_triangles)
+    return mesh_smp
 
 
-def subdivide_remesh(mesh, numSubdivisions):
-    if numSubdivisions == 0:
-        return mesh
-    else:
-        numSubdivisions = numSubdivisions - 1
-        new_vertices, new_faces = trimesh.remesh.subdivide(
-            mesh.vertices, mesh.faces, face_index=None, vertex_attributes=None, return_index=False)
-        newMesh: trimesh.Trimesh = trimesh.Trimesh(
-            vertices=new_vertices, faces=new_faces)
-        return subdivide_remesh(newMesh, numSubdivisions)
+def visualizeMesh(mesh) -> None:
+    print(
+        f'Simplified mesh has {len(mesh.vertices)} vertices and {len(mesh.triangles)} triangles'
+    )
+    o3d.visualization.draw_geometries([mesh])
 
 
-def subdivide_to_size(mesh, numSubdivisions):
-    if numSubdivisions == 0:
-        return mesh
-    else:
-        numSubdivisions = numSubdivisions - 1
-        new_vertices, new_faces = trimesh.remesh.subdivide_to_size(
-            mesh.vertices, mesh.faces, max_edge=0.01, max_iter=10, return_index=False)
-        newMesh: trimesh.Trimesh = trimesh.Trimesh(
-            vertices=new_vertices, faces=new_faces)
-        return subdivide_to_size(newMesh, numSubdivisions)
-
-def exportToGLTF(mesh, filename='assets1\\'+MESH_NAME+'-remesh'+'.glb'):
-    mesh.export(filename)
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Error: No .glb file specified')
@@ -80,20 +66,17 @@ if __name__ == '__main__':
     # get file size in byte, MB, KB, GB
     print(MESH_NAME+'\'s file size is :', getFileSize(fileAddr, roundDigit=2))
 
-    mesh: trimesh.Trimesh = trimesh.load(fileAddr, force="mesh")
-    new_vertices, new_faces = trimesh.remesh.subdivide(
-        mesh.vertices, mesh.faces, face_index=None, vertex_attributes=None, return_index=False)
+    # load mesh
+    origMesh = o3d.data.BunnyMesh()
+    # preprocess the mesh
+    mesh = o3d.io.read_triangle_mesh(origMesh.path)
+    mesh.compute_vertex_normals()
+
+    visualizeMesh(mesh)
+    mesh_smp_VC = simplifyMeshVC(mesh, degree=64)
+    visualizeMesh(mesh_smp_VC)
     
-    for i in range(3):
-        meshC = (subdivide(mesh, i+1))
-        print("Subdivide %(times)d times, Faces#: %(faces)d, Vertices#: %(vertices)d" % {
-              'times': i, 'faces': len(meshC.faces), 'vertices': len(meshC.vertices)})
-    for i in range(3):
-        meshC = (subdivide_remesh(mesh, i+1))
-        print("Remsh.subdivide %(times)d times, Faces#: %(faces)d, Vertices#: %(vertices)d" % {
-            'times': i, 'faces': len(meshC.faces), 'vertices': len(meshC.vertices)})
-        
-    for i in range(3):
-        meshC = (subdivide_to_size(mesh, i+1))
-        print("Remsh.subdivide_to_size %(times)d times, Faces#: %(faces)d, Vertices#: %(vertices)d" % {
-            'times': i, 'faces': len(meshC.faces), 'vertices': len(meshC.vertices)})
+    num_of_triangles = len(mesh.triangles)
+    degree = 10
+    mesh_smp_QD = simplifyMeshQD(mesh, num_of_triangles=int(num_of_triangles/degree))
+    visualizeMesh(mesh_smp_QD)
