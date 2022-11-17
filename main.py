2e32913f23d693efcd912fcc51d318e5fe705f4d
HarveyLijh <jli783@wisc.edu>
2022-11-17 14:22:24 -0500

    finalize visualization

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ MESH_NAME = ''
 
 def getFileSize(filename, roundDigit=2) -> float:
     size = os.path.getsize(filename)
-    if(size == 0):
+    if (size == 0):
         return '0 byte'
-    if(size == 1):
+    if (size == 1):
         return '1 byte'
-    if(size > 1024):
+    if (size > 1024):
         size = size/1024
-        if(size > 1024):
+        if (size > 1024):
             size = size/1024
             return str(round(size, roundDigit))+' MB'
         else:
             return str(round(size, roundDigit))+' KB'
-    if(size > 1024):
+    if (size > 1024):
         size = size/1024
         return str(round(size, roundDigit))+' MB'
-    if(size > 1024):
+    if (size > 1024):
         size = size/1024
         return str(round(size, roundDigit))+' GB'
     return str(round(size, roundDigit))+' bytes'
@@ -32,6 +32,8 @@ def getFileSize(filename, roundDigit=2) -> float:
 # simplify mesh using Vertex clustering
 # lower the degree value, more the simplification
 # http://www.open3d.org/docs/release/tutorial/geometry/mesh.html#Mesh-simplification
+
+
 def simplifyMeshVC(mesh, degree=32) -> o3d.geometry.TriangleMesh:
     voxel_size = max(mesh.get_max_bound() - mesh.get_min_bound()) / degree
     mesh_smp = mesh.simplify_vertex_clustering(
@@ -42,6 +44,8 @@ def simplifyMeshVC(mesh, degree=32) -> o3d.geometry.TriangleMesh:
 # simplify mesh using Quadric Decimation
 # lower the num_of_triangle value, more the simplification
 # http://www.open3d.org/docs/release/tutorial/geometry/mesh.html#Mesh-simplification
+
+
 def simplifyMeshQD(mesh, num_of_triangles) -> o3d.geometry.TriangleMesh:
     mesh_smp = mesh.simplify_quadric_decimation(
         target_number_of_triangles=num_of_triangles)
@@ -52,11 +56,14 @@ def visualizeMesh(mesh) -> None:
     print(
         f'Simplified mesh has {len(mesh.vertices)} vertices and {len(mesh.triangles)} triangles'
     )
-    o3d.visualization.draw_geometries([mesh])
+    if str(sys.argv[-1]) == '-g':
+        o3d.visualization.draw_geometries([mesh])
+    else:
+        o3d.visualization.draw([mesh])
 
 
 if __name__ == '__main__':
-    
+
     # check arguments and file existence
     if len(sys.argv) < 2:
         raise Exception('Error: No .glb file specified!')
@@ -64,32 +71,29 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
         raise Exception('Error: No operation specified!')
     operation = str(sys.argv[2])
-    
+
     if os.path.exists(fileAddr) == False:
         raise Exception('Error: File not found!')
-        
+
     # get file size in byte, MB, KB, GB
     print(fileAddr+'\'s file size is :', getFileSize(fileAddr, roundDigit=2))
 
     # load and preprocess mesh
     print(f'Loading mesh {fileAddr} ...')
     mesh = o3d.io.read_triangle_mesh(fileAddr)
-    
-    
+
     # origMesh = o3d.data.BunnyMesh()
     # mesh = o3d.io.read_triangle_mesh(origMesh.path)
-    
-    
-    mesh.compute_vertex_normals()
 
-    
+    mesh.compute_vertex_normals()
     # handle operations
     if operation == 'original':
         o3d.io.write_triangle_mesh('original.glb', mesh)
         print(f'Visualizing original mesh...')
         visualizeMesh(mesh)
         exit(0)
-        
+
+
     if operation == 'VC':
         if len(sys.argv) < 4:
             raise Exception('Error: VC operation has no degree specified!')
@@ -99,17 +103,17 @@ if __name__ == '__main__':
         print(f'Visualizing simplified mesh using simplify_vertex_clustering ...')
         visualizeMesh(mesh_smp_VC)
         exit(0)
-    
+
     if operation == 'QD':
         if len(sys.argv) < 4:
             raise Exception('Error: QD operation has no degree specified!')
         degree = int(sys.argv[3])
         num_of_triangles = len(mesh.triangles)
-        mesh_smp_QD = simplifyMeshQD(mesh, num_of_triangles=int(num_of_triangles/degree))
+        mesh_smp_QD = simplifyMeshQD(
+            mesh, num_of_triangles=int(num_of_triangles/degree))
         o3d.io.write_triangle_mesh(f'QD_{degree}.glb', mesh_smp_QD)
         print(f'Visualizing simplified mesh using simplify_quadric_decimation ...')
         visualizeMesh(mesh_smp_QD)
         exit(0)
-        
-    
+
     raise Exception('Error: Operation not found')
--- runLoci.sh
+++ runLoci.sh
@@ -8,7 +8,8 @@ python main.py b.glb
 # mac
 conda activate lociEnv
 cd /Users/harveyli/Documents/GitHub/Get_Metadata_from_glb-gltf
-python main.py ./assets1/hat.glb VC 10
-python main.py ./assets1/hat.glb VC 32
-python main.py ./assets1/hat.glb QD 12
-python main.py ./assets1/hat.glb QD 50
+python main.py ./assets1/hat.glb original -g
+python main.py ./assets1/hat.glb VC 10 -g
+python main.py ./assets1/hat.glb VC 32 -g
+python main.py ./assets1/hat.glb QD 12 -g
+python main.py ./assets1/hat.glb QD 50 -g
